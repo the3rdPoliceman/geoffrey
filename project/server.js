6e6f7f3aede31e5b6156816d67d51a60a4f364a2
the3rdPoliceman <gabbymoore@gmail.com>
2023-05-16 23:27:38 +0200

    wiring requests to the public folder by configuring express in server.js

--- project/server.js
+++ project/server.js
@@ -1,8 +1,21 @@
+const express = require('express');
+const path = require('path');
 const schedule = require('node-schedule');
 const auctionScraper = require('./src/tasks/auctionScraper');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve the static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Schedule the auctionScraper task to run daily at 08:00
 schedule.scheduleJob('0 8 * * *', () => {
     console.log('Running auctionScraper task...');
     auctionScraper();
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Geoffrey is running on port ${port}`);
 });
